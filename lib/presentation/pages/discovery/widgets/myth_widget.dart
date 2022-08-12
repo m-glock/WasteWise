e106e4f923ca7bd5842efa04f394bb8f79423e59
m-glock <mareike.glock@t-online.de>
2022-08-12 17:13:53 +0200

    added expanding widget for myths

--- lib/presentation/pages/discovery/widgets/myth_widget.dart
+++ lib/presentation/pages/discovery/widgets/myth_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recycling_app/presentation/pages/discovery/widgets/myth_list_widget.dart';
 
 class MythWidget extends StatefulWidget {
   const MythWidget({Key? key}) : super(key: key);
@@ -10,6 +11,37 @@ class MythWidget extends StatefulWidget {
 class _MythWidgetState extends State<MythWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Myth"),);
+    return Container(
+      margin: const EdgeInsets.only(top: 15),
+      child: ListView(
+        children: const [
+          //TODO replace with actual text
+          MythListWidget(
+              question: "Question 1",
+              answer:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                  "Donec porta orci convallis arcu aliquet, quis fringilla sapien semper. "
+                  "Suspendisse dapibus nibh et lacus scelerisque sollicitudin. "
+                  "Nunc pellentesque aliquet massa pulvinar accumsan. "
+                  "Duis vitae eros vel urna sollicitudin malesuada a vel sapien. "),
+          MythListWidget(
+              question: "Question 2",
+              answer:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                  "Donec porta orci convallis arcu aliquet, quis fringilla sapien semper. "
+                  "Suspendisse dapibus nibh et lacus scelerisque sollicitudin. "
+                  "Nunc pellentesque aliquet massa pulvinar accumsan. "
+                  "Duis vitae eros vel urna sollicitudin malesuada a vel sapien. "),
+          MythListWidget(
+              question: "Question 2",
+              answer:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                  "Donec porta orci convallis arcu aliquet, quis fringilla sapien semper. "
+                  "Suspendisse dapibus nibh et lacus scelerisque sollicitudin. "
+                  "Nunc pellentesque aliquet massa pulvinar accumsan. "
+                  "Duis vitae eros vel urna sollicitudin malesuada a vel sapien. "),
+        ],
+      ),
+    );
   }
 }
