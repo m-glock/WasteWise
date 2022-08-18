7a716d3fe0cc735f093b452c15df068abc1d8f06
m-glock <mareike.glock@t-online.de>
2022-08-18 14:31:08 +0200

    improved UI of search page

--- lib/presentation/pages/search/search_page.dart
+++ lib/presentation/pages/search/search_page.dart
@@ -13,66 +13,73 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  List recentlySearched = ["Styropor", "Asche", "Holz"];
-  List oftenSearched = ["Korken", "Kleiderbügel", "Knochen"];
+  List<String> recentlySearched = ["Styropor", "Asche", "Holz"];
+  List<String> oftenSearched = ["Korken", "Kleiderbügel", "Knochen"];
+
+  Widget _barcodeScannerButton() {
+    return SizedBox(
+      width: 160,
+      child: ElevatedButton(
+        child: Row(
+          children: const [
+            Icon(FontAwesomeIcons.barcode, size: 12),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Text('Barcode Scanner'),
+            )
+          ],
+        ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const BarcodeScanPage()),
+          );
+        },
+      ),
+    );
+  }
+
+  List<Widget> _itemList(List<String> listOfNames) {
+    return listOfNames
+        .map((element) => Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child:
+                  Text(element, style: Theme.of(context).textTheme.bodyText1),
+            ))
+        .toList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SearchBar(),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: SizedBox(
-                width: 170,
-                child: ElevatedButton(
-                  child: Row(
-                    children: const [
-                      Icon(FontAwesomeIcons.barcode, size: 12),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 5),
-                        child: Text('Barcode Scanner'),
-                      )
-                    ],
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const BarcodeScanPage()),
-                    );
-                  },
-                ),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SearchBar(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 10, 40),
+            child: _barcodeScannerButton(),
+          ),
+          Column(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //TODO replace with actual items and links
+                  Text(Languages.of(context)!.recentlySearched,
+                      style: Theme.of(context).textTheme.headline3),
+                  const Padding(padding: EdgeInsets.only(bottom: 15)),
+                  ..._itemList(recentlySearched),
+                  const Padding(padding: EdgeInsets.only(bottom: 25)),
+                  Text(Languages.of(context)!.oftenSearched,
+                      style: Theme.of(context).textTheme.headline3),
+                  const Padding(padding: EdgeInsets.only(bottom: 15)),
+                  ..._itemList(oftenSearched),
+                ],
               ),
-            ),
-            Column(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //TODO replace with actual items and links
-                    Text(Languages.of(context)!.recentlySearched,
-                        style: Theme.of(context).textTheme.headline2),
-                    const Padding(padding: EdgeInsets.only(bottom: 5)),
-                    ...recentlySearched
-                        .map((element) => Text(element,
-                            style: Theme.of(context).textTheme.bodyText1))
-                        .toList(),
-                    const Padding(padding: EdgeInsets.only(bottom: 25)),
-                    Text(Languages.of(context)!.oftenSearched,
-                        style: Theme.of(context).textTheme.headline2),
-                    const Padding(padding: EdgeInsets.only(bottom: 5)),
-                    ...oftenSearched
-                        .map((element) => Text(element,
-                            style: Theme.of(context).textTheme.bodyText1))
-                        .toList(),
-                  ],
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
+        ],
       ),
     );
   }
