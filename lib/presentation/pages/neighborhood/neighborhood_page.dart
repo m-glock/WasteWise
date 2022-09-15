eec85d516e1772747b2cca2b99fee76f1dd1c34b
m-glock <mareike.glock@t-online.de>
2022-09-15 14:16:49 +0200

    refactored neighborhood feed widget to include ask question

--- lib/presentation/pages/neighborhood/neighborhood_page.dart
+++ lib/presentation/pages/neighborhood/neighborhood_page.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:recycling_app/presentation/i18n/languages.dart';
 import 'package:recycling_app/presentation/pages/neighborhood/widgets/neighborhood_feed_widget.dart';
-
-import '../../util/custom_icon_button.dart';
-import '../../util/data_holder.dart';
-import '../../util/graphl_ql_queries.dart';
 
 class NeighborhoodPage extends StatefulWidget {
   const NeighborhoodPage({Key? key}) : super(key: key);
@@ -18,7 +12,6 @@ class NeighborhoodPage extends StatefulWidget {
 
 class _NeighborhoodPageState extends State<NeighborhoodPage> {
   bool _isAuthenticated = false;
-  final TextEditingController controller = TextEditingController();
 
   @override
   void initState() {
@@ -33,78 +26,11 @@ class _NeighborhoodPageState extends State<NeighborhoodPage> {
     });
   }
 
-  void _createForumPost() async {
-    String forumTypeId = DataHolder.forumEntryTypesById.entries
-        .firstWhere((element) => element.value.typeName == "Question")
-        .key;
-    Map<String, dynamic> inputVariables = {
-      "userId": (await ParseUser.currentUser())?.objectId,
-      "forumEntryTypeId": forumTypeId,
-      "questionText": controller.text,
-    };
-
-    GraphQLClient client = GraphQLProvider.of(context).value;
-    QueryResult<Object?> result = await client.query(
-      QueryOptions(
-        document: gql(GraphQLQueries.createForumPost),
-        variables: inputVariables,
-      ),
-    );
-
-    String snackBarText =
-    result.hasException || !result.data?["createForumEntries"]
-        ? Languages.of(context)!.cpPostUnsuccessfulText
-        : Languages.of(context)!.cpPostSuccessfulText;
-
-    controller.text = "";
-    FocusManager.instance.primaryFocus?.unfocus();
-
-    ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(snackBarText))
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _isAuthenticated
-          ? Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                CustomIconButton(
-                  onPressed: () => {},
-                  padding: const EdgeInsets.symmetric(horizontal: 5),
-                  icon: const Icon(FontAwesomeIcons.filter),
-                ),
-                const Padding(padding: EdgeInsets.only(bottom: 10)),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: controller,
-                        keyboardType: TextInputType.multiline,
-                        maxLines: null,
-                        autocorrect: false,
-                        decoration: InputDecoration(
-                          fillColor: Theme.of(context).colorScheme.surface,
-                          labelText: "Content",
-                          filled: true,
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    const Padding(padding: EdgeInsets.symmetric(horizontal: 3)),
-                    CustomIconButton(
-                      padding: EdgeInsets.zero,
-                      icon: const Icon(Icons.send),
-                      onPressed: () => _createForumPost(),
-                    ),
-                  ],
-                ),
-                const Padding(padding: EdgeInsets.only(bottom: 10)),
-                const NeighborhoodFeedWidget(),
-              ],
-            )
+          ? const NeighborhoodFeedWidget()
           : Center(
               child:
                   Text(Languages.of(context)!.neighborhoodNotAuthenticatedText),
--- lib/presentation/pages/neighborhood/widgets/neighborhood_feed_widget.dart
+++ lib/presentation/pages/neighborhood/widgets/neighborhood_feed_widget.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:recycling_app/presentation/util/database_classes/forum_entry_type.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../../i18n/languages.dart';
 import '../../../util/constants.dart';
+import '../../../util/custom_icon_button.dart';
 import '../../../util/data_holder.dart';
 import '../../../util/graphl_ql_queries.dart';
 import 'forum_entry_widget.dart';
@@ -17,6 +21,7 @@ class NeighborhoodFeedWidget extends StatefulWidget {
 
 class _NeighborhoodFeedWidgetState extends State<NeighborhoodFeedWidget> {
   String? municipalityId;
+  final TextEditingController controller = TextEditingController();
 
   @override
   void initState() {
@@ -33,13 +38,48 @@ class _NeighborhoodFeedWidgetState extends State<NeighborhoodFeedWidget> {
     });
   }
 
+
+  void _createForumPost() async {
+    String forumTypeId = DataHolder.forumEntryTypesById.entries
+        .firstWhere((element) => element.value.typeName == "Question")
+        .key;
+    Map<String, dynamic> inputVariables = {
+      "userId": (await ParseUser.currentUser())?.objectId,
+      "forumEntryTypeId": forumTypeId,
+      "questionText": controller.text,
+    };
+
+    GraphQLClient client = GraphQLProvider.of(context).value;
+    QueryResult<Object?> result = await client.query(
+      QueryOptions(
+        document: gql(GraphQLQueries.createForumPost),
+        variables: inputVariables,
+      ),
+    );
+
+    String snackBarText =
+    result.hasException || !result.data?["createForumEntries"]
+        ? Languages.of(context)!.cpPostUnsuccessfulText
+        : Languages.of(context)!.cpPostSuccessfulText;
+
+    setState(() {
+      controller.text = "";
+    });
+
+    FocusManager.instance.primaryFocus?.unfocus();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(snackBarText))
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return municipalityId == null
         ? const Center(child: CircularProgressIndicator())
         : Query(
             options: QueryOptions(
-              fetchPolicy: FetchPolicy.noCache,
+                fetchPolicy: FetchPolicy.networkOnly,
                 document: gql(GraphQLQueries.getForumEntries),
                 variables: {"municipalityId": municipalityId}),
             builder: (QueryResult result,
@@ -53,7 +93,41 @@ class _NeighborhoodFeedWidgetState extends State<NeighborhoodFeedWidget> {
 
               List<dynamic> forumEntries = result.data?["getForumEntries"];
 
-              return Expanded(
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  CustomIconButton(
+                    onPressed: () => {},
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                    icon: const Icon(FontAwesomeIcons.filter),
+                  ),
+                  const Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: controller,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: null,
+                          autocorrect: false,
+                          decoration: InputDecoration(
+                            fillColor: Theme.of(context).colorScheme.surface,
+                            labelText: "Content",
+                            filled: true,
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      const Padding(padding: EdgeInsets.symmetric(horizontal: 3)),
+                      CustomIconButton(
+                        padding: EdgeInsets.zero,
+                        icon: const Icon(Icons.send),
+                        onPressed: () => _createForumPost(),
+                      ),
+                    ],
+                  ),
+                  const Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Expanded(
                     child: ListView.builder(
                       shrinkWrap: true,
                       itemCount: forumEntries.length,
@@ -72,6 +146,8 @@ class _NeighborhoodFeedWidgetState extends State<NeighborhoodFeedWidget> {
                         );
                       },
                     ),
+                  ),
+                ],
               );
             },
     );
