64b73336c988da74aed55e09b66704da86504ed7
m-glock <mareike.glock@t-online.de>
2022-09-21 20:06:10 +0200

    use provider to update neighborhood page if user is logged in

--- lib/presentation/pages/neighborhood/neighborhood_page.dart
+++ lib/presentation/pages/neighborhood/neighborhood_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:provider/provider.dart';
 import 'package:recycling_app/presentation/i18n/languages.dart';
 import 'package:recycling_app/presentation/pages/neighborhood/widgets/neighborhood_feed_widget.dart';
+
+import '../../util/database_classes/user.dart';
 
 class NeighborhoodPage extends StatefulWidget {
   const NeighborhoodPage({Key? key}) : super(key: key);
@@ -11,30 +13,17 @@ class NeighborhoodPage extends StatefulWidget {
 }
 
 class _NeighborhoodPageState extends State<NeighborhoodPage> {
-  bool _isAuthenticated = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkIfAuthenticated();
-  }
-
-  void _checkIfAuthenticated() async {
-    ParseUser? currentUser = await ParseUser.currentUser();
-    setState(() {
-      _isAuthenticated = currentUser != null;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _isAuthenticated
-          ? const NeighborhoodFeedWidget()
-          : Center(
-              child:
-                  Text(Languages.of(context)!.neighborhoodNotAuthenticatedText),
-            ),
-    );
+    return Consumer<User>(builder: (BuildContext context, User user, child) {
+      return Scaffold(
+        body: user.currentUser != null
+            ? const NeighborhoodFeedWidget()
+            : Center(
+                child: Text(
+                    Languages.of(context)!.neighborhoodNotAuthenticatedText),
+              ),
+      );
+    });
   }
 }
